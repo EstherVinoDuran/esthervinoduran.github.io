d4e5f7c81dd72870a36cc4e9ff15675b49e2dfeb
EverVino <evervino00@gmail.com>
2025-07-23 19:30:16 -0400

    fix typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://esthervinoduran.github.io"
+	site: "https://esthervinoduran.github.io",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
